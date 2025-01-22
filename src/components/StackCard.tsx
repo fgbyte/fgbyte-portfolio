289d7848c58cbbf74f199a022bc8c3c536af4f90
fgbyte <fer.garciabayon@gmail.com>
2025-01-21 21:17:59 -0500

    refactor: replace StackCard.astro with StackCard.tsx, update Icons and Social Links

---
+++ src/components/StackCard.tsx
@@ -0,0 +1,21 @@
+type Props = {
+	href: string;
+	text: string;
+	icon: string;
+};
+
+export default function StackCard({ href, text, icon }: Props) {
+	return (
+		<a
+			href={href}
+			target="_blank"
+			class="w-fit px-3 py-2 group rounded border flex gap-2 items-center border-neutral-200 dark:border-neutral-700 hover:bg-neutral-100 hover:dark:bg-neutral-800 blend"
+			rel="noreferrer"
+		>
+			<div class="w-5 h-5">{icon}</div>
+			<span class="text-sm capitalize text-neutral-500 dark:text-neutral-400 group-hover:text-black group-hover:dark:text-white blend">
+				{text}
+			</span>
+		</a>
+	);
+}
