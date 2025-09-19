7d01ee4bcd9969cf87e455f176c8889d57f2b562
fgbyte <fer.garciabayon@gmail.com>
2025-09-19 09:33:33 -0400

    feat(icons, homepage): Add Turborepo and Vercel icons, update tech stack
    Adds new SVG icons for Turborepo and Vercel to the component library. These icons are then integrated into the homepage's tech stack section to reflect a broader range of technologies.
    
    This commit also includes:
    - Refactoring and reordering of the tech stack on the homepage, including the addition of Nativewind.
    - Updating the "Software Developer" title to "FullStack Developer" in the about section.
    - Minor adjustment to the viewBox property of the AstroIcon for improved rendering.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -60,7 +60,7 @@ export const AstroIcon = (props: any) => {
 	return (
 		<svg
 			{...props}
-			viewBox="0 0 85 107"
+			viewBox="0 0 80 102"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
@@ -107,7 +107,7 @@ export const NextjsIcon = (props: any) => (
 export const StorybookIcon = (props: any) => (
 	<svg
 		viewBox="-31.5 0 319 319"
-		version="1.1"
+		sion="1.1"
 		xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
 		preserveAspectRatio="xMidYMid"
@@ -568,6 +568,50 @@ export const NeonIcon = (props: any) => (
 	</svg>
 );
 
+export const Turborepo = (props: any) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		{...props}
+	>
+		<title>{"turbo"}</title>
+		<path
+			fill="#E4E4E7"
+			d="M12 6c-3.308 0-6 2.692-6 6s2.692 6 6 6 6-2.692 6-6-2.692-6-6-6zm0 9.105a3.105 3.105 0 110-6.21 3.105 3.105 0 010 6.21z"
+		/>
+		<path
+			fill="url(#paint0_linear_22527_86)"
+			d="M12.537 4.425V1.998C17.81 2.28 22 6.647 22 11.991c0 5.343-4.19 9.709-9.463 9.992v-2.427c3.93-.28 7.043-3.565 7.043-7.566 0-4-3.113-7.285-7.043-7.565zM6.26 16.952a7.556 7.556 0 01-1.833-4.417H2a9.965 9.965 0 002.543 6.134l1.717-1.717zm5.187 5.03v-2.426a7.549 7.549 0 01-4.419-1.833L5.312 19.44a9.964 9.964 0 006.134 2.543h.002z"
+		/>
+		<defs>
+			<linearGradient
+				id="paint0_linear_22527_86"
+				x1="12.929"
+				x2="3.093"
+				y1="3.404"
+				y2="13.24"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#0096FF" />
+				<stop offset="1" stop-color="#FF1E56" />
+			</linearGradient>
+		</defs>
+	</svg>
+);
+
+export const Vercel = (props: any) => (
+	<svg
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		{...props}
+	>
+		<title>{"vercel"}</title>
+		<path d="M12 4L21 19.5886H3L12 4Z" fill="#64748B" />
+	</svg>
+);
+
 export const Icons = [
 	ReactjsIcon,
 	DrizzleIcon,
@@ -596,4 +640,6 @@ export const Icons = [
 	TrpcIcon,
 	HonoIcon,
 	NeonIcon,
+	Turborepo,
+	Vercel,
 ];
